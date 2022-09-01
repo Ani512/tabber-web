f7a4aec085f9499a70dd19d02809f7be629712b0
Anirudh Umarji <55306116+Ani512@users.noreply.github.com>
2022-09-01 17:56:09 -0500

    adding github link in login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -2,13 +2,15 @@ import { connect } from 'react-redux';
 import '../styles/css/App.css';
 import { startLogin } from '../actions/authActions';
 import { Button } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const Login = ( props ) =>
 (
   <div className="App">
     <header className="App-header">
-      <h2>Tabber</h2>
+      <h1>Tabber</h1>
       <Button variant="contained" className="btn login-btn" onClick={ props.startLogin } color="primary">Login</Button>
+      <a style={{ marginTop: '5%' }} href="https://github.com/Ani512/tabber-web" target="blank"><GitHubIcon sx={{ fontSize: 50 }} /></a>
     </header>
   </div>
 );
